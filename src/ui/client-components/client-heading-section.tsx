a6255b6a13ebba259005998d94c29d6ad03385c0
bobp-id <bobp@id.com.au>
2025-08-28 18:54:05 +1000

    update

--- src/ui/client-components/client-heading-section.tsx
+++ src/ui/client-components/client-heading-section.tsx
@@ -1,6 +1,5 @@
 import type { Lga } from "@/page-data/types/Lga";
 import MyHeader from "../core-components/my-header";
-import MyWrapper from "../core-components/my-wrapper";
 import ClientHeading from "./client-heading";
 import ClientBanner from "./client-banner";
 import ClientProducts from "./client-products";
