885b38ca24d11cf1fd60c6e6369d36642785c0e0
Bob Pham <bobpham.tdp@gmail.com>
2026-02-01 00:46:13 +1100

    re-design for mobile

--- src/features/home/components/ProductTab.tsx
+++ src/features/home/components/ProductTab.tsx
@@ -4,7 +4,7 @@ import { products } from "@/page-data/products"
 
 const MyProductTab = () => {
     return (
-        <div className={`max-w-${MAX_WIDTH} mx-auto px-2 sm:px-2 lg:px-4 py-2 bg-white z-10 relative flex flex-wrap gap-1`}>
+        <div className={`max-w-${MAX_WIDTH} mx-auto px-2 sm:px-2 lg:px-4 py-2 bg-white z-10 relative flex overflow-x-auto whitespace-nowrap gap-1 scrollbar-hide`}>
             <ProductButton products={products} />
         </div>
     )
