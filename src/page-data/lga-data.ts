ecbb21059e89186b6ec198f658423213c4327798
Bob Pham <bobpham.tdp@gmail.com>
2026-03-25 17:38:35 +1100

    reduce the number of demo clients

--- src/page-data/lga-data.ts
+++ src/page-data/lga-data.ts
@@ -5,12 +5,6 @@ const baseUrl = 'https://housing.id.com.au'
 
 const lgaData: Lga[] = [
   {
-    "id": "20740",
-    "name": "Bass Coast Shire",
-    "alias": "basscoast",
-    "image": "/static/b01468aa921533d1a851abf27c1134dc/deb99/20740.png"
-  },  
-  {
     "id": "10750",
     "name": "Blacktown City Council",
     "alias": "blacktown",
@@ -22,25 +16,6 @@ const lgaData: Lga[] = [
     "alias": "adelaide",
     "image": "/static/665313beab7ae3329158177dfb25c01f/deb99/40070.png"
   },
-  {
-    "id": "20910",
-    "name": "City of Bayside",
-    "alias": "bayside",
-    "image": "/static/c2c08a5abae6c642433828290d8bec6b/deb99/20910.png"
-  },
-  {
-    "id": "21610",
-    "name": "City of Casey",
-    "alias": "casey",
-    "image": "/static/8ea0757de9edba8bdbef0da41b2b72cd/deb99/21610.png"
-  },
-  {
-    "id": "24600",
-    "name": "City of Melbourne",
-    "alias": "melbourne",
-    "image": "/static/6f6aa3851f2ca73a089493f9214bbf79/deb99/24600.png"
-  },
-
   {
     "id": "22170",
     "name": "Frankston City",
